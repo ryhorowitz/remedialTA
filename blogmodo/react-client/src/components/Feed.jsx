c611215cd856a4b14eee9edb683718b0c9d3d579
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-08-27 14:34:54 -0400

    Using the Moment.js library to display relative time.

--- blogmodo/react-client/src/components/Feed.jsx
+++ blogmodo/react-client/src/components/Feed.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 
-// const samplePosts: {
-//   _id: string;
-//   title: string;
-//   author: string;
-//   imageUrl: string;
-//   createdAt: string;
-//   body: string;
-//   views: number;
-// }[]
-//map props
-// return each <li> dynamically
-
+// createdAt: '2017-11-14T05:57:26.037Z',
+// moment(sample.createdAt); is an object.
 const Feed = (props) => {
+  //const relativeTime = moment.updateLocale('en');
+
   const listItems = props.samples.map( (sample, i) => {
+    let timeStamp = moment(sample.createdAt).fromNow();
     return <li className="feed-list-item" key={sample._id}>
       <div className="feed-list-item-title" onClick={props.handleClick}>{sample.title}</div>
-      <div className="feed-list-item-byline"><span className="feed-list-item-byline-author">{sample.author}</span>{sample.createdAt}</div>
+      <div className="feed-list-item-byline"><span className="feed-list-item-byline-author">{sample.author}</span>{console.log('time is', timeStamp)} {timeStamp} </div>
       <img src={sample.imageUrl} onClick={props.handleClick} className="feed-list-item-image" />
       <span className="feed-list-item-lede">{sample.body} </span>
     </li>;
